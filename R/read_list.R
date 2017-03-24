1c3315cc7fec4ffc76f3da073bf6ef47e75ce0f7
Frédéric Olland <frederic.olland@gmail.com>
2017-03-24 16:03:01 +0000

    add parallelisation

--- R/read_list.R
+++ R/read_list.R
@@ -2,6 +2,7 @@
 #' @param list_of_datasets A list of datasets (names of datasets are strings)
 #' @param read_func A function, the read function to use to read the data
 #' @param ... Further arguments passed to read_func
+#' @param parallelize Parallelize the code
 #' @return Returns a list of the datasets
 #' @export
 #' @examples
@@ -10,22 +11,38 @@
 #' list_of_datasets <- list.files(pattern = "*.csv")
 #' list_of_loaded_datasets <- read_list(list_of_datasets, read_func = read.csv)
 #' }
-read_list <- function(list_of_datasets, read_func, ...){
-
-	stopifnot(length(list_of_datasets)>0)
-
-	read_and_assign <- function(dataset, read_func){
-		dataset_name <- as.name(dataset)
-		dataset_name <- read_func(dataset, ...)
-	}
-
-	# invisible is used to suppress the unneeded output
-	output <- invisible(
-		purrr::map(list_of_datasets,
-			   read_and_assign, read_func = read_func))
-
-	# Remove the ".csv" at the end of the data set names
-	names_of_datasets <- c(unlist(strsplit(list_of_datasets, "[.]"))[c(T, F)])
-	names(output) <- names_of_datasets
-	return(output)
+read_list <- function(list_of_datasets,  read_func, ..., parallelize = TRUE){
+  
+  stopifnot(length(list_of_datasets)>0)
+  
+  read_and_assign <- function(dataset, read_func){
+    dataset_name <- as.name(dataset)
+    dataset_name <- read_func(dataset, ...)
+  }
+  if (parallelize == TRUE) {
+    clusters <- parallel::detectCores() %>%
+      parallel::makeCluster() 
+    doParallel::registerDoParallel(clusters)
+    output <- invisible(
+      pbapply::pblapply(list_of_datasets,
+                        read_and_assign,
+                        read_func = read_func, 
+                        cl = clusters)
+    )
+    parallel::stopCluster(clusters)
+  } else if (parallelize == FALSE) {
+    # invisible is used to suppress the unneeded output
+    output <- invisible(
+      pbapply::pblapply(list_of_datasets,
+                        read_and_assign,
+                        read_func = read_func))
+  } 
+  
+  # Remove what's after the "." at the end of the data set names and what's before any / for url files.
+  names_of_datasets <- list_of_datasets %>% str_replace_all(".*/|\\..*", "") 
+  names(output) <- names_of_datasets
+  return(output)
 }
+
+
+
