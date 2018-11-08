c1b8a0be1cc584cedab4efd86e2d6bf880c71925
Bruno Rodrigues <bruno@brodrigues.co>
2018-11-08 22:44:38 +0100

    added a color

--- R/theme_blog.R
+++ R/theme_blog.R
@@ -9,7 +9,8 @@ theme_blog <- function(){
         plot.title = element_text(colour = "white"),
         panel.grid = element_line(colour = "#425d65"),
         axis.text = element_text(colour = "white"),
-        axis.title = element_text(colour = "white"))
+        axis.title = element_text(colour = "white"),
+        strip.text = element_text(colour = "white"))
 }
 
 #' fill scale that works well with blog
@@ -18,7 +19,7 @@ scale_fill_blog <- function(){
   scale_fill_manual(values = c("#82518c", "#0f4150", "#5160a0",
                  "#88720e", "#bec3b8", "#8c6451",
                  "#5b8c51", "#880e61", "#0e2488",
-                 "#880e20", "#d33682", "#0e8876"))
+                 "#880e20", "#d33682", "#0e8876", "#859900"))
 }
 
 #' color scale that works well with blog
@@ -27,7 +28,7 @@ scale_color_blog <- function(){
   scale_color_manual(values = c("#82518c", "#0f4150", "#5160a0", #old nice, color too #002b36, but a bit dark
                  "#88720e", "#bec3b8", "#8c6451",
                  "#5b8c51", "#880e61", "#0e2488",
-                 "#880e20", "#d33682", "#0e8876"))
+                 "#880e20", "#d33682", "#0e8876", "#859900"))
 }
 
 #' continuous scale that works well with blog
