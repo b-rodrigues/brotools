30c3461ed0bbf294215fbaa59d596e55137f4a8c
Bruno Rodrigues <bruno@brodrigues.co>
2019-05-19 20:47:52 +0200

    started take.R

---
+++ R/take.R
@@ -0,0 +1,11 @@
+#' Take the first n elements of a list
+#' @param n An integer. The number of elements to take from the list.
+#' @param ls A list.
+#' @return A list containg the first n` elements of `ls`.
+#' @description 
+#' @export
+#' @examples
+ take <- function(ls){
+
+
+}
